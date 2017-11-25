95ed48409e07c25ec974622a60b9c1e547f31d33
Marcel Mráz <445662@muni.cz>
2017-11-25 11:46:27 +0100

    adding references and windsor castle

--- WebApi/Global.asax.cs
+++ WebApi/Global.asax.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using System.Web.Http.Dispatcher;
 using System.Web.Routing;
+using Castle.Windsor;
 
 namespace WebApi
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
+        private readonly IWindsorContainer container = new WindsorContainer();
+
+
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            BootstrapContainer();
+        }
+        private void BootstrapContainer()
+        {
+            container.Install(new WebApiInstaller());
+            container.Install(new BusinessLayerInstaller());
+
+            GlobalConfiguration.Configuration.Services
+                .Replace(typeof(IHttpControllerActivator), new WindsorCompositionRoot(container));
+        }
+
+        public override void Dispose()
+        {
+            container.Dispose();
+            base.Dispose();
         }
     }
 }
