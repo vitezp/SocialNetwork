c2c2483df364f004e769366556b8849895634885
Marcel Mráz <445662@muni.cz>
2017-11-25 12:15:25 +0100

    BL - adding castle installer

---
+++ SocialNetworkBL/Config/BusinessLayerInstaller.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.Resolvers.SpecializedResolvers;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using SocialNetworkBL.Facades.Common;
+using SocialNetworkBL.QueryObjects.Common;
+using SocialNetworkBL.Services.Common;
+
+namespace SocialNetworkBL.Config
+{
+    public class BusinessLayerInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        { 
+            container.Register(
+                Classes.FromThisAssembly()
+                    .BasedOn(typeof(QueryObjectBase<,,,>))
+                    .WithServiceBase()
+                    .LifestyleTransient(),
+
+                Classes.FromThisAssembly()
+                    .BasedOn<ServiceBase>()
+                    .WithServiceDefaultInterfaces()
+                    .LifestyleTransient(),
+
+                Classes.FromThisAssembly()
+                    .BasedOn(typeof(FacadeBase<,,>))
+                    .WithServiceBase()
+                    .LifestyleTransient(),
+
+                Component.For<IMapper>()
+                    .Instance(new Mapper(new MapperConfiguration(MappingConfig.ConfigureMapping)))
+                    .LifestyleSingleton()
+            );
+
+            container.Kernel.Resolver.AddSubResolver(new CollectionResolver(container.Kernel));
+        }
+    }
+}
